d22b0f94826b2bb7f4c8b04c84340e9530ac4810
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2023-09-30 19:48:17 +0530

    15th June

--- src/test/java/Advance/Bug.java
+++ src/test/java/Advance/Bug.java
@@ -15,6 +15,9 @@ import java.util.*;
  */
 public class Bug {
 
+    String url;
+    String cookie;
+
     @Test(priority = 1)
     public void loginJira() throws IOException, ParseException {
 
@@ -22,7 +25,7 @@ public class Bug {
         FileReader fr = new FileReader("/home/nandkumar/Videos/15th June API/15th_JuneBatch/src/main/resources/globalValues.properties");
         Properties pr =new Properties();
         pr.load(fr);
-       String url = pr.getProperty("url");
+        url = pr.getProperty("url");
 
        //Read the body from the json file.
         FileReader jsonFile = new FileReader("/home/nandkumar/Videos/15th June API/15th_JuneBatch/src/main/java/inputJsons/login.json");
@@ -36,8 +39,31 @@ public class Bug {
 
         //To read the json respone and find cookie value.
         JSONObject js = new JSONObject(response.asString());
-        String cookie = "JSESSIONID="+js.getJSONObject("session").get("value").toString();
+        cookie = "JSESSIONID="+js.getJSONObject("session").get("value").toString();
         System.out.println(cookie);
+    }
+
+    @Test(priority = 2)
+    public void createBug() throws IOException, ParseException {
+
+      //  note - We are getting the URL from global to understand more please refer the read properties cod in login
+
+        //To get the request body (BUG)
+        FileReader fr =new FileReader("/home/nandkumar/Videos/15th June API/15th_JuneBatch/src/main/java/inputJsons/CreateBug.json");
+        JSONParser jp = new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+
+        //Create bug code
+        Response response = RestAssured.given().baseUri(url).body(requestBody).header("Cookie", cookie).contentType(ContentType.JSON)
+                .when().post("/rest/api/2/issue")
+                .then().log().all().extract().response();
+
+        //To get the issue id for PUT- Delete - Get call.
+        JSONObject js = new JSONObject(response.asString());
+        String issueId = js.get("key").toString();
+        System.out.println(issueId);
+
+
     }
 
 }
